f85941e0fc97242b420fb200d61d566e838311b6
licheng <2501093733@qq.com>
2024-03-19 14:23:47 +0800

    编写文档!

--- src/main/java/com/demo/controller/IndexController.java
+++ src/main/java/com/demo/controller/IndexController.java
@@ -4,10 +4,7 @@ import ch.qos.logback.core.pattern.color.BoldYellowCompositeConverter;
 import com.demo.entity.R;
 import com.demo.entity.User;
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 
@@ -34,12 +31,12 @@ public class IndexController {
         return R.okData(map);
     }
 
-    @RequestMapping("/formData")
+    @PostMapping("/formData")
     public Object formData( User user){
         return R.okData(user);
     }
 
-    @RequestMapping("/raw")
+    @PostMapping("/raw")
     public Object raw(@RequestBody String body){
         return R.okData(body);
     }
